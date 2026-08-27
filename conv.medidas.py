72ded46dfc418173c2391e9c016fd3216f224b68
Gabriela Queiroz <gabrielaqueiroznunes2501@gmail.com>
2026-08-27 17:02:04 -0300

    area e perimetro

---
+++ conv.medidas.py
@@ -0,0 +1,5 @@
+metros = float(input("Qual o tamanho em metrôs?"))
+centimetros = metros * 100
+milimetros = metros * 1000
+print("O tamanho de metros em centímetros é:", centimetros, "cm")
+print("O tamanho de metros em milímetros é:", milimetros, "mm")
---
+++ peri_area.py
@@ -0,0 +1,8 @@
+largura = float(input("Qual é a largura do retângulo?"))
+altura = float(input("Qual é a altura do retângulo?"))
+
+area = largura * altura
+perimetro = 2 * (largura+altura)
+
+print("A área é:", area)
+print("O perímetro é:", perimetro)
