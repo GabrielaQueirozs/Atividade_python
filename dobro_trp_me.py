af410bd8e6b4e7498414dbf3a23296afdcacb40d
Gabriela Queiroz <gabrielaqueiroznunes2501@gmail.com>
2026-08-27 16:33:47 -0300

    dobro,metade,triplo

---
+++ dobro_trp_me.py
@@ -0,0 +1,9 @@
+numero = int(input("Escreva um número:"))
+dobro = numero * 2
+triplo = numero * 3
+metade = numero /2
+
+print("numero:", numero )
+print("dobro:", dobro)
+print("triplo:", triplo)
+print("metade:", metade) 
